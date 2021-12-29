873555169c71379987a9bc0826fd8db4d7b088da
uedarikuto <k283292@kansai-u.ac.jp>
2021-12-29 18:09:18 +0900

    8-1のとりまコミット

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <div class='post'>
+                <h2 class='title'>Title_1</h2>
+                <p class='body'> This_is_a_sumple_body</p>
+            </div>
+        </div>
+    </body>
+</html>
